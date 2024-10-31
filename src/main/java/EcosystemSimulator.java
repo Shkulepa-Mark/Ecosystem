607c406f49ce952404def96fd2efc94ab71cc4e6
shkulepa mark <skulepamark@gmail.com>
2024-11-01 00:33:38 +0300

    Implement logSimulation method

--- src/main/java/EcosystemSimulator.java
+++ src/main/java/EcosystemSimulator.java
@@ -5,6 +5,7 @@ public class EcosystemSimulator {
     private List<Animal> animals;
     private List<Plant> plants;
     private Enviroment enviroment;
+    private FileManager fileManager;
 
     public EcosystemSimulator() {
         animals = new ArrayList<>();
@@ -24,16 +25,39 @@ public class EcosystemSimulator {
         for (Animal animal : animals) {
             animal.eat();
             animal.increaseAgeOneYear();
+            animal.starve();
         }
 
         for (Plant plant : plants) {
             plant.grow();
         }
 
+        enviroment.changeWeather();
+
         System.out.println("Simulation completed");
     }
 
-    public static void main(String[] args) {
-        EcosystemSimulator ecosystemSimulator = new EcosystemSimulator();
+    private void logSimulation() {
+        String filename = "Ecosystem_Log.txt";
+        StringBuilder logData = new StringBuilder();
+        logData.append("Environment: Temperature = ").append(enviroment.getTemperature()).append(", Humidity = ").append(enviroment.getHumidity()).append("\n");
+
+        for (Animal animal : animals) {
+            logData.append(animal.toString()).append("\n");
+        }
+
+        for (Plant plant : plants) {
+            logData.append(plant.toString()).append("\n");
+        }
+
+        fileManager.saveToFile(filename, logData.toString());
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public List<Plant> getPlants() {
+        return plants;
     }
 }
